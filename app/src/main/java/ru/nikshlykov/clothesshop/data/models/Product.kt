ab6bef582cb50be0c56f73b5f313fad47c19acec
Nik Shlykov <nikshlykovdev@gmail.com>
2020-08-10 11:46:15 +0300

    init

---
+++ app/src/main/java/ru/nikshlykov/clothesshop/data/models/Product.kt
@@ -0,0 +1,4 @@
+package ru.nikshlykov.clothesshop.data.models
+
+data class Product(val name: String, val price: Int, val size: String, val color: String,
+                   val material: String, val description: String)
