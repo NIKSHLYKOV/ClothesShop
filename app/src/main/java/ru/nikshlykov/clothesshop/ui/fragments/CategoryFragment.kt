16aa6b92b3d3d3737b549908b7edfc0351f103d7
Nik Shlykov <nikshlykovdev@gmail.com>
2020-09-01 12:42:08 +0300

    add CategoryFragment

---
+++ app/src/main/java/ru/nikshlykov/clothesshop/ui/fragments/CategoryFragment.kt
@@ -0,0 +1,18 @@
+package ru.nikshlykov.clothesshop.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.nikshlykov.clothesshop.R
+
+class CategoryFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+}
